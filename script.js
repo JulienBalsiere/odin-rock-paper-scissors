3113780247ae15bb692d77ffb881cca655b8f48e
Julien Balsiere <julien.balsiere@gmail.com>
2026-06-29 11:52:29 +0200

    RPS game run correctly for a 5 rounds game

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// Fonction qui permet de récupérer le choix du computer
+// Récupérer le choix du computer
 function getComputerChoice() {
-  // La fonction doit retourner aléatoirement paper, rock ou scissors
-  // On prend un nb avec math.random entre 0 et 1, qu'on multiplie par 3 et qu'on arrondie à l'entier supérieur pour avoir 1, 2 ou 3.
+
   const random = Math.floor(Math.random() * 3);
 
   if (random === 0) return "Rock";
@@ -9,50 +8,68 @@ function getComputerChoice() {
   return "Scissors";
 }
 
-// Fonction qui permet de récupérer le choix de l'utilisateur
+// Récupérer l'input utilisateur
 function getHumanChoice() {
   let playerChoice = window.prompt("Choose between Rock, Paper or Scissors");
   return playerChoice;
 }
 
-// Amorcer les variables qui traquent le score des joueurs.
+// Création des compteurs de score.
 let humanScore = 0;
 let computerScore = 0;
 
-// Fonction qui permet de jouer un round avec en paramètres le choix des joueurs
-// La fonction incrémente le score du joueur gagnant et annonce le vainqueur du round.
-
+// Fonctionnement d'un round
 function playRound(humanChoice, computerChoice) {
-  // prendre la valeur de human et computer et la passer en lowercase.
+  // Transformer les inputs en lowercase
   const human = humanChoice.toLowerCase();
   const computer = computerChoice.toLowerCase();
-  // comparer ces valeurs et faire les possibilité dans des ifs.
-  // Retourner le gagnant
-  // incrémenter le score
+
   if (human === "rock" && computer === "paper") {
     computerScore += 1;
-    return "You lose! Paper beats Rock";
+    return "You lose this round! Paper beats Rock";
   } else if (human === "rock" && computer === "scissors") {
     humanScore += 1;
-    return "You win ! Rock beats Scissors";
+    return "You win this round! Rock beats Scissors";
   } else if (human === "paper" && computer === "scissors") {
     computerScore += 1;
-    return "You lose! Scissors beats Paper";
+    return "You lose this round! Scissors beats Paper";
   } else if (human === "paper" && computer === "rock") {
     humanScore += 1;
-    return "You win ! Paper beats Rock";
+    return "You win this round! Paper beats Rock";
   } else if (human === "scissors" && computer === "rock") {
     computerScore += 1;
-    return "You lose ! Rock beats Scissors";
+    return "You lose this round! Rock beats Scissors";
   } else if (human === "scissors" && computer === "paper") {
     humanScore += 1;
-    return "You win ! Scissors beats paper";
+    return "You win this round! Scissors beats paper";
   } else {
     return "It's a tie !"
   }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+// Fonction pour compater les scores et déclarer le vainqueur de la partie
+function winnerIs(humanScore, computerScore) {
+  if (humanScore > computerScore) {
+    return "Congratulations, you win the game !";
+  } else if (humanScore < computerScore) {
+    return "You lose, computer win the game.";
+  } else {
+    return "It's a tie";
+  }
+}
 
-playRound(humanSelection, computerSelection);
+// Fonction pour lancer une partie en 5 rounds.
+function playGame() {
+  // Utilisation de variables globales.
+  humanScore = 0;
+  computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    const result = playRound(humanSelection, computerSelection);
+    console.log(result);
+    console.log(`Round ${i + 1} Your score is ${humanScore}, Computer socres is ${computerScore}`) ;
+  }
+  console.log(winnerIs(humanScore, computerScore));
+}
